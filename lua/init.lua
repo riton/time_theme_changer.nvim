680652feca49826e32f2d6283cacc39071e91845
Rémi Ferrand <remi.ferrand@cc.in2p3.fr>
2023-02-07 00:56:16 +0100

    WIP

--- lua/init.lua
+++ lua/init.lua
@@ -1,14 +1,10 @@
 local M = {}
 
 local function background_job(user_config)
-  math.randomseed(os.time())
   local timer = vim.loop.new_timer()
   -- timer:start(2000, 1800000, vim.schedule_wrap(function()
-  timer:start(2000, 3000, vim.schedule_wrap(function()
+  timer:start(2000, 1800000, vim.schedule_wrap(function()
     local hour = tonumber(os.date("%H"))
-    print("Setting colorscheme: ", hour)
-
-    hour = math.random(0, 24)
 
     local scheme_mode = "dark"
     if hour >= 8 and hour <= 19
@@ -22,7 +18,7 @@ local function background_job(user_config)
       colorscheme = "tokyonight-night"
     end
 
-    print("Setting colorscheme to ", colorscheme, " hour is ", hour)
+    -- print("Setting colorscheme to ", colorscheme, ", hour is ", hour)
 
     vim.api.nvim_command("colorscheme " .. colorscheme)
   end))
